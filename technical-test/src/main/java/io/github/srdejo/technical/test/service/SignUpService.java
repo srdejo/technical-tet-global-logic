0ecac75ff65e85b550cba2c4db5d300824b4d479
Daniel Jimenez <srdejo@gmail.com>
2025-09-07 22:45:22 -0500

    add api to global project

--- technical-test/src/main/java/io/github/srdejo/technical/test/service/SignUpService.java
+++ technical-test/src/main/java/io/github/srdejo/technical/test/service/SignUpService.java
@@ -13,14 +13,14 @@ public interface SignUpService {
      * <ul>
      *   <li>Validates that no user already exists with the given email.</li>
      *   <li>Encodes the provided password using the configured {@code PasswordEncoder}.</li>
-     *   <li>Creates and persists a new {@link User} entity from the request data.</li>
+     *   <li>Creates and persists a new {@link io.github.srdejo.technical.test.entities.User} entity from the request data.</li>
      *   <li>Generates a JWT token for the newly created user.</li>
      * </ul>
      * </p>
      *
      * @param request the sign-up request containing user information (email, password, name, phones, etc.).
      * @return a {@link UserResponse} containing the persisted user details and a JWT token.
-     * @throws UserAlreadyExistsException if a user with the given email is already registered.
+     * @throws io.github.srdejo.technical.test.exceptions.UserAlreadyExistsException if a user with the given email is already registered.
      */
     UserResponse register(@Valid SignUpRequest request);
 }
